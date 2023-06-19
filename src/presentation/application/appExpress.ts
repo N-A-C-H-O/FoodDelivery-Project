495688ede0a766408ce8f390f29b10e5641a1af9
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-19 19:28:38 -0300

    feat: add presentation folder with applicatoin factories middlewares and routes folders

---
+++ src/presentation/application/appExpress.ts
@@ -0,0 +1,32 @@
+import express from "express";
+
+import businessRouter from "../routes/businessRouter.js"
+import ordersRouter from "../routes/ordersRouter.js"
+import usersRouter from "../routes/usersRouter.js"
+
+import errorHandler from "../middlewares/errorHandler.ts";
+
+class AppExpress {
+    private app = express();
+    private PORT = process.env.NODE_PORT;
+
+    init() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+    }
+
+    build() {
+        this.app.use("/api/business", businessRouter);
+        this.app.use("/api/users", usersRouter);
+        this.app.use("/api/orders", ordersRouter);
+        this.app.use(errorHandler);
+    }
+
+    listen() {
+        return this.app.listen(this.PORT, () => {
+            console.log(`Server listening on port ${this.PORT}`);
+        });
+    }
+}
+
+export default AppExpress;
